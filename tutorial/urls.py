d4daeb42b1d03e7c084badc8113972791ad73ed4
navneet35371 <navneet.anand@hindustantimes.com>
2015-01-14 11:19:39 +0530

    Tut - 2

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('snippets.urls')),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
