e233b52a8f77abf2b70f0079a67cbac7e08977ca
navneet35371 <navneet.anand@hindustantimes.com>
2015-01-16 16:12:26 +0530

    Tut -3 | using generic views

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,14 +1,10 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('snippets.urls')),
-)
+urlpatterns = [
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
